4d7b7a6b90f87f9623daa182b1e5d5e428b2c0c1
Neoknight5 <anuradhajayale8@gmail.com>
2026-07-18 19:59:48 +0530

    Update modretion_TIMEOUT.py

--- modretion_TIMEOUT.py
+++ modretion_TIMEOUT.py
@@ -2,71 +2,58 @@ import discord
 from discord.ext import commands
 
 import datetime
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print("Bot Online!")
+
 
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello!")
 
+
 @bot.command()
-async def timeout(ctx,member:discord.Member,message):
-	if ctx.author.guild_permissions.administrator:
-		ind = len(message) - 1
-		if message[ind] == "s":
-			n = message[0:ind]
-			val = int(n)
-			current =  datetime.now().astimezone()
-			next = current + timedelta(seconds=val)
-			await member.timeout(next)
-			await ctx.send(f"{member.mention} had timeout :(")
-	elif not ctx.guild.me.guild_permissions.kick_members:
-	   await ctx.send("❌ I don't have permission to kick members. \n :(")	
-	if ctx.author.guild_permissions.administrator:
-		if message[ind] == "m":
-			n = message[0:ind]
-			val = int(n)
-			current =  datetime.now().astimezone()
-			next = current + timedelta(minutes=val)
-			await member.timeout(next)
-			await ctx.send(f"{member.mention} had timeout :(")
-	elif not ctx.guild.me.guild_permissions.kick_members:
-	   await ctx.send("❌ I don't have permission to kick members. \n :(")	
-	if ctx.author.guild_permissions.administrator:
-		if message[ind] == "h":
-			n = message[0:ind]
-			val = int(n)
-			current =  datetime.now().astimezone()
-			next = current + timedelta(hours=val)
-			await member.timeout(next)
-			await ctx.send(f"{member.mention} had timeout :(")
-	elif not ctx.guild.me.guild_permissions.kick_members:
-	   await ctx.send("❌ I don't have permission to kick members. \n :(")
-	if ctx.author.guild_permissions.administrator:
-		if message[ind] == "d":
-			n = message[0:ind]
-			val = int(n)
-			current =  datetime.now().astimezone()
-			next = current + timedelta(days=val)
-			await member.timeout(next)
-			await ctx.send(f"{member.mention} had timeout :(")
-	elif not ctx.guild.me.guild_permissions.kick_members:
-	   await ctx.send("❌ I don't have permission to kick members. \n :(")	
-	if ctx.author.guild_permissions.administrator:
-		if message[ind] == "w":
-			n = message[0:ind]
-			val = int(n)
-			current =  datetime.now().astimezone()
-			next = current + timedelta(weeks=val)
-			await member.timeout(next)
-			await ctx.send(f"{member.mention} had timeout :(")
-	elif not ctx.guild.me.guild_permissions.kick_members:
-	   await ctx.send("❌ I don't have permission to kick members. \n :(")
+async def timeout(ctx, member: discord.Member, message):
+    if not ctx.author.guild_permissions.administrator:
+        await ctx.send("❌ You don't have permission to use this command.")
+        return
+
+    if not ctx.guild.me.guild_permissions.moderate_members:
+        await ctx.send("❌ I don't have permission to timeout members.\n:(")
+        return
+
+    ind = len(message) - 1
+    unit = message[ind]
+    n = message[:ind]
+
+    if not n.isdigit():
+        await ctx.send("❌ Invalid time format.")
+        return
+
+    val = int(n)
+    current = datetime.now().astimezone()
+
+    if unit == "s":
+        next = current + timedelta(seconds=val)
+    elif unit == "m":
+        next = current + timedelta(minutes=val)
+    elif unit == "h":
+        next = current + timedelta(hours=val)
+    elif unit == "d":
+        next = current + timedelta(days=val)
+    elif unit == "w":
+        next = current + timedelta(weeks=val)
+    else:
+        await ctx.send("❌ Invalid time unit. Use s, m, h, d or w.")
+        return
+
+    await member.timeout(next)
+    await ctx.send(f"{member.mention} has been timed out :(")
