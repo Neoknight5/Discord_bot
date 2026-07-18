55125b08d95485567c50960c5de9bb6367d5a855
Neoknight5 <anuradhajayale8@gmail.com>
2026-07-18 18:10:55 +0530

    Refactor kick command for improved permission checks

--- Moderator_KICK.py
+++ Moderator_KICK.py
@@ -13,17 +13,15 @@ async def on_ready():
     print("Bot Online!")
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Hello!")
-@bot.command()
 async def kick(ctx,member:discord.Member,*,message):
-	if ctx.author.guild_permissions.administrator:
-		await member.kick(reason=f"{message} \n:(")
-	elif not ctx.guild.me.guild_permissions.kick_members:
-	   await ctx.send("❌ I don't have permission to kick members. \n :(")
-	   return
-	else:
-		await ctx.send(f"your dont had permission to kick anyone !! \n - only administrator (admin) have it ^-^ ")
+    if not ctx.guild.me.guild_permissions.kick_members:
+        await ctx.send("❌ I don't have permission to kick members. \n :(")
+    elif ctx.author.guild_permissions.administrator:
+        await member.kick(reason=f"{message} \n:(")
+        await ctx.send(f"✅ {member} has been kicked from the server.:( \n - reason: ```{message}```")  
+    else:
+        await ctx.send(f"your dont had permission to kick anyone !! \n - only administrator (admin) have it ^-^ ")
+	
 	
 
 bot.run("^-^")
